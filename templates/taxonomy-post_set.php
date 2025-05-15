aaafd5c5a8e22fe4320fe0536ebc0fac86f6cbc9
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-05-15 10:36:03 +0200

    used a new variable to query posts
    used a new variable to query posts

--- templates/taxonomy-post_set.php
+++ templates/taxonomy-post_set.php
@@ -9,7 +9,7 @@ get_header();
 
 // Fetch the global $wp_query object to access posts in the current query
 global $wp_query;
-$posts = $wp_query->posts;
+$queried_posts = $wp_query->posts; // Use a custom variable
 ?>
 <div class="mh-wrapper mh-clearfix">
     <div id="main-content" class="mh-loop mh-content" role="main">
@@ -21,23 +21,23 @@ $posts = $wp_query->posts;
             </div>
         </header>
         <?php
-        // Check if $posts is not empty
-        if ( ! empty( $posts ) ) :
-            foreach ( $posts as $post ) :
-                setup_postdata( $post ); // Important for using template tags
+        // Check if $queried_posts is not empty
+        if ( ! empty( $queried_posts ) ) :
+            foreach ( $queried_posts as $queried_post ) :
+                setup_postdata( $queried_post ); // Use the custom variable
                 ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <article id="post-<?php echo $queried_post->ID; ?>" <?php post_class(); ?>>
                     <?php
                     if ( has_post_thumbnail() ) {
                         the_post_thumbnail( 'medium' );
                     }
                     ?>
-                    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <h2 class="entry-title"><a href="<?php echo get_permalink( $queried_post->ID ); ?>"><?php echo esc_html( get_the_title( $queried_post->ID ) ); ?></a></h2>
                     <div class="meta">
-                        <time datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
+                        <time datetime="<?php echo esc_attr( get_the_date( 'c', $queried_post->ID ) ); ?>"><?php echo esc_html( get_the_date( '', $queried_post->ID ) ); ?></time>
                     </div>
                     <div class="entry-summary">
-                        <?php the_excerpt(); ?>
+                        <?php echo esc_html( get_the_excerpt( $queried_post->ID ) ); ?>
                     </div>
                 </article>
                 <?php
@@ -47,5 +47,7 @@ $posts = $wp_query->posts;
             get_template_part( 'content', 'none' );
         endif;
         ?>
-    </div><?php get_sidebar(); ?>
-</div><?php get_footer(); ?>
+    </div>
+    <?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
